22419336b7a1fbb4b6fddbae2c05922fe71d0a6b
Jenny Kim <kimj1905@gmail.com>
2021-03-04 18:16:24 -0500

    push

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length
+        return collection[i] if yield(collection[i])
+        # if the block return true, return the argument we passed into yield
+        # if the block return false, it continues to the next element in the array
+        i += 1
+    end
 end
